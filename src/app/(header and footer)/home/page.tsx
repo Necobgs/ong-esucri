bb60eff08a8c8eaba63699a3a0bd05f4a52b9953
Necobgs <emanuelborgesdev@gmail.com>
2025-05-01 15:05:06 -0300

    fontes adicionadas na página home

--- src/app/(header and footer)/home/page.tsx
+++ src/app/(header and footer)/home/page.tsx
@@ -26,7 +26,7 @@ export default function Home(){
        <section className="min-h-[100dvh] w-full flex flex-col justify-around pt-[5%] pl-[10%] pr-[10%]">
         <article className="flex justify-around items-center gap-[10%] w-full h-[80vh]">
             <div className="w-[50%] flex gap-[50px] flex-col">
-            <h1 className="font-bold text-[64px] text-amber-400 ff-SourGummy">
+            <h1 className="font-bold text-[64px] text-amber-400 ff-SourGummy-bold">
                 Lorem ipsum dolor sit amet, consectetur
             </h1>
             <p className="font-medium text-[16px] ff-NunitoSans">
@@ -37,10 +37,10 @@ export default function Home(){
                 rhoncus, iaculis nunc lobortis, dapibus arcu.
             </p>
             <div className="flex justify-center items-center gap-6 text-white">
-                <button className="bg-[#866AFF] p-3 rounded-3xl">
+                <button className="bg-[#866AFF] p-3 rounded-3xl ff-SourGummy-bold">
                 QUERO ME VOLUNTARIAR
                 </button>
-                <button className="bg-[#866AFF] p-3 rounded-3xl">
+                <button className="bg-[#866AFF] p-3 rounded-3xl ff-SourGummy-bold">
                 QUERO FAZER UMA DOAÇÃO
                 </button>
             </div>
@@ -62,27 +62,27 @@ export default function Home(){
         </div>
     </section>
         <section className="h-[60dvh] w-full bg-amber-400 flex items-center justify-center flex-row mt-10 mb-10 gap-20">
-            <div className="text-center w-1/4 text-3xl rounded-t-full rounded-br-full bg-amber-200 h-3/4 flex items-center justify-center">Frase motivadora</div>
-            <div className="w-2/4">
-                <h2 className="text-6xl">Sobre o instituto</h2>  
-                <p className="text-1xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mi neque, finibus quis luctus sed, scelerisque id ante. In sagittis nec est vitae pellentesque. Suspendisse potenti. Aliquam massa libero, ullamcorper quis aliquam dignissim, tempus a massa. Cras sed sapien rhoncus, iaculis nunc lobortis, dapibus arcu.</p>
+            <div className="text-center w-1/4 text-5xl rounded-t-full rounded-br-full bg-amber-200 h-3/4 flex items-center justify-center ff-SourGummy-bold">Frase motivadora</div>
+            <div className="w-4/8">
+                <h2 className="text-6xl mb-10 ff-SourGummy-bold">Sobre o instituto</h2>  
+                <p className="text-1xl ff-NunitoSans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mi neque, finibus quis luctus sed, scelerisque id ante. In sagittis nec est vitae pellentesque. Suspendisse potenti. Aliquam massa libero, ullamcorper quis aliquam dignissim, tempus a massa. Cras sed sapien rhoncus, iaculis nunc lobortis, dapibus arcu.</p>
             </div>
         </section>
         <section className="w-full flex items-center justify-center flex-col gap-20 mt-20 mb-20">
-            <h2 className="text-6xl">Missão, Visão e Valores</h2>
+            <h2 className="text-6xl ff-SourGummy-bold">Missão, Visão e Valores</h2>
             <article className="flex flex-row w-3/4 gap-20">
                 <p className="w-1/3 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mi neque, finibus quis luctus sed, scelerisque id ante. In sagittis nec est vitae pellentesque. Suspendisse potenti. Aliquam massa libero, ullamcorper quis aliquam dignissim, tempus a massa. Cras sed sapien rhoncus, iaculis nunc lobortis, dapibus arcu.</p>
                 <p className="w-1/3 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mi neque, finibus quis luctus sed, scelerisque id ante. In sagittis nec est vitae pellentesque. Suspendisse potenti. Aliquam massa libero, ullamcorper quis aliquam dignissim, tempus a massa. Cras sed sapien rhoncus, iaculis nunc lobortis, dapibus arcu.</p>
                 <p className="w-1/3 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mi neque, finibus quis luctus sed, scelerisque id ante. In sagittis nec est vitae pellentesque. Suspendisse potenti. Aliquam massa libero, ullamcorper quis aliquam dignissim, tempus a massa. Cras sed sapien rhoncus, iaculis nunc lobortis, dapibus arcu.</p>
             </article>
             <span className="flex items-center justify-center flex-col gap-5">
-                <p className="text-center text-3xl">Nossa história completa</p>
-                <button className="rounded-3xl bg-[#866AFF] pt-2 pb-2 pl-4 pr-4 text-white">Quero conhecer</button>
+                <p className="text-center text-3xl ff-SourGummy-bold">Nossa história completa</p>
+                <button className="rounded-3xl bg-[#866AFF] pt-2 pb-2 pl-4 pr-4 text-white ff-SourGummy-bold">Quero conhecer</button>
             </span>
 
         </section>
         <section className="w-full bg-sky-300 flex items-center justify-center flex-col gap-20 p-20 box-border">
-            <h2 className="text-6xl">Nossos cursos</h2>
+            <h2 className="text-6xl ff-SourGummy-bold">Nossos cursos</h2>
             <article className="flex flex-wrap justify-center gap-x-30 gap-y-20"> 
                 <CourseCard icon="icon de exemplo" title="Lorem Ipsum dolor Sit" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mi neque, finibus quis luctus sed, scelerisque id ante. In sagittis nec est vitae pellentesque. Suspendisse potenti. Aliquam massa libero, ullamcorper quis aliquam dignissim, tempus a massa. Cras sed sapien rhoncus, iaculis nunc lobortis, dapibus arcu."></CourseCard>
                 <CourseCard icon="icon de exemplo" title="Lorem Ipsum dolor Sit" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mi neque, finibus quis luctus sed, scelerisque id ante. In sagittis nec est vitae pellentesque. Suspendisse potenti. Aliquam massa libero, ullamcorper quis aliquam dignissim, tempus a massa. Cras sed sapien rhoncus, iaculis nunc lobortis, dapibus arcu."></CourseCard>
@@ -92,7 +92,7 @@ export default function Home(){
             </article>
         </section>   
         <section className="p-10">
-            <div className="text-center w-full p-10 text-6xl">
+            <div className="text-center w-full p-10 text-6xl ff-SourGummy-bold">
                 Relatos
             </div>
             <div className="h-160">
@@ -100,21 +100,19 @@ export default function Home(){
             </div>
         </section>
         <section className="w-full flex items-center justify-center flex-col gap-20 p-20 box-border">
-            <div className="text-center w-full p-10 text-6xl">
+            <div className="text-center w-full p-10 text-6xl ff-SourGummy-bold">
                 Nossas conquistas
             </div>
-            <div className="h-160">
             <article className="flex flex-wrap justify-center gap-x-30 gap-y-20"> 
                 <CourseCard icon="icon de exemplo" title="Lorem Ipsum dolor Sit" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mi neque, finibus quis luctus sed, scelerisque id ante. In sagittis nec est vitae pellentesque. Suspendisse potenti. Aliquam massa libero, ullamcorper quis aliquam dignissim, tempus a massa. Cras sed sapien rhoncus, iaculis nunc lobortis, dapibus arcu."></CourseCard>
                 <CourseCard icon="icon de exemplo" title="Lorem Ipsum dolor Sit" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mi neque, finibus quis luctus sed, scelerisque id ante. In sagittis nec est vitae pellentesque. Suspendisse potenti. Aliquam massa libero, ullamcorper quis aliquam dignissim, tempus a massa. Cras sed sapien rhoncus, iaculis nunc lobortis, dapibus arcu."></CourseCard>
                 <CourseCard icon="icon de exemplo" title="Lorem Ipsum dolor Sit" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mi neque, finibus quis luctus sed, scelerisque id ante. In sagittis nec est vitae pellentesque. Suspendisse potenti. Aliquam massa libero, ullamcorper quis aliquam dignissim, tempus a massa. Cras sed sapien rhoncus, iaculis nunc lobortis, dapibus arcu."></CourseCard>
             </article>
-            </div>
         </section>
         <section className="w-full flex items-center justify-center flex-col p-20 box-border">
             <div className="w-full p-10 flex items-center justify-center flex-col mb-20">
-                <h2 className="text-6xl text-center">Empresas parceiras</h2>
-                <h3 className="text-1xl text-center text-gray-400">Lorem ipsum dolor sit amet, consectetur</h3>
+                <h2 className="text-6xl text-center ff-SourGummy-bold">Empresas parceiras</h2>
+                <h3 className="text-1xl text-center  ff-NunitoSans">Lorem ipsum dolor sit amet, consectetur</h3>
             </div>
             <div className="flex items-center justify-around w-10/12 mb-20">
                 <span>icon 1</span>
@@ -125,11 +123,31 @@ export default function Home(){
                 <span>icon 6</span>
                 <span>icon 7</span>
             </div>
-            <p className="text-2xl text-wrap text-center mb-20">Lorem ipsum dolor sit amet, consectetur adipiscing<br/> elit. Fusce mi neque, finibus quis luctus sed, </p>
-            <span className="mb-5 text-2xl">Se torne um parceiro!</span>
-            <button className="bg-black p-3 rounded-3xl text-white">
+            <p className="text-2xl text-wrap text-center mb-20 ff-SourGummy-bold">Lorem ipsum dolor sit amet, consectetur adipiscing<br/> elit. Fusce mi neque, finibus quis luctus sed, </p>
+            <span className="mb-5 text-4xl ff-SourGummy-bold">Se torne um parceiro!</span>
+            <button className="bg-black p-3 rounded-3xl text-white ff-SourGummy-bold">
                 QUERO CONHECER!
             </button>
+        </section>
+        <section className="p-50 flex items-center justify-around bg-gray-200">
+            <div className="flex flex-col items-center justify-center w-1/3">
+                <span className="mb-5 text-6xl ff-SourGummy-bold text-center">Seja um<br/>voluntário</span>
+                <button className="bg-black pt-3 pb-3 pl-4 pr-4 rounded-4xl text-white ff-SourGummy-bold text-2xl">
+                    QUERO CONHECER!
+                </button>
+            </div>
+            <div className="flex flex-col items-center justify-center w-1/3">
+                <span className="mb-5 text-6xl ff-SourGummy-bold text-center">Fale<br/>conosco</span>
+                <button className="bg-black pt-3 pb-3 pl-4 pr-4 rounded-4xl text-white ff-SourGummy-bold text-2xl">
+                    QUERO CONHECER!
+                </button>
+            </div>
+            <div className="flex flex-col items-center justify-center w-1/3">
+                <span className="mb-5 text-6xl ff-SourGummy-bold text-center">Faça uma<br/>doação</span>
+                <button className="bg-black pt-3 pb-3 pl-4 pr-4 rounded-4xl text-white ff-SourGummy-bold text-2xl">
+                    QUERO CONHECER!
+                </button>
+            </div>
         </section>
     </>
  )   
--- src/components/course-card/course-card.tsx
+++ src/components/course-card/course-card.tsx
@@ -8,8 +8,8 @@ export default function CourseCard(props:Props){
     return (
         <article className="flex flex-col justify-center items-center gap-10 w-1/5">
             <div>{props.icon}</div>
-            <div className="text-center ">{props.title}</div>
-            <div className="text-center ">{props.text}</div>
+            <div className="text-center ff-SourGummy-bold text-3xl">{props.title}</div>
+            <div className="text-center ff-NunitoSanss">{props.text}</div>
         </article>
     )
 }
